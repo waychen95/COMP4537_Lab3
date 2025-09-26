9e404799640b9eb7656d6017cacb27af2f8ec10a
Wayne Chen <wchen206@my.bcit.ca>
2025-09-26 16:51:22 -0700

    updated read and write file logic

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -48,18 +48,25 @@ class Server {
         const file_name = "file.txt"
         console.log(file_name, text)
         fs.appendFile(file_name, text + "\n", (err) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type':'text/html'})
+                res.end(template.append_error)
+                return
+            }
             res.writeHead(200, {'Content-Type':'text/html'})
-            err ? res.end(template.append_error) : res.end(template.append_success)
+            res.end(template.append_success)
         })
     }
 
     handleReadFile(res) {
         const file_name = "file.txt"
         fs.readFile(file_name, "utf-8", (err, data) => {
-            res.writeHead(200, {'Content-Type':'text/html'})
             if (err) {
+                res.writeHead(500, {'Content-Type':'text/html'})
                 res.end(template.readfile_error.replace("%1", file_name))
+                return
             }
+            res.writeHead(200, {'Content-Type':'text/html'})
             res.end(template.readfile_text.replace("%1", data))
         })
     }
